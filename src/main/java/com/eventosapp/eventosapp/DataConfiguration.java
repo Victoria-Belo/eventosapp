c77ba1613d11f2db11e2d9a2cdc1f175eec551ff
Anesidora II <vickbelo17@gmail.com>
2022-10-07 21:43:40 -0300

    configurando postgresql

--- src/main/java/com/eventosapp/eventosapp/DataConfiguration.java
+++ src/main/java/com/eventosapp/eventosapp/DataConfiguration.java
@@ -1,7 +1,11 @@
 package com.eventosapp.eventosapp;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +18,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
-	 static final String URL = "jdbc:mysql://localhost:3306/eventos_bd?serverTimezone=UTC"; //indica o caminho do banco de dados
+	 /*static final String URL = "jdbc:mysql://localhost:3306/eventos_bd?serverTimezone=UTC"; //indica o caminho do banco de dados
 	 static final String USER = "root"; // aqui vai o nome usuario que vc quer acessar
 	 static final String PASS = "root"; // aqui a senha do seu banco
 	 
@@ -37,6 +41,21 @@ public class DataConfiguration {
 		  adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
 		  adapter.setPrepareConnection(true);
 		  return adapter; 
-	 }
+	 }*/
+	 @Bean
+	    public BasicDataSource dataSource() throws URISyntaxException {
+	        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	        String username = dbUri.getUserInfo().split(":")[0];
+	        String password = dbUri.getUserInfo().split(":")[1];
+	        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+	        BasicDataSource basicDataSource = new BasicDataSource();
+	        basicDataSource.setUrl(dbUrl);
+	        basicDataSource.setUsername(username);
+	        basicDataSource.setPassword(password);
+
+	        return basicDataSource;
+	    }
 }
 
